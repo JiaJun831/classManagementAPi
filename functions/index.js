37d9bda13bc0f6835a47f63cbfba6c3ed2fa0c86
JiaJun831 <64371500+JiaJun831@users.noreply.github.com>
2022-02-20 23:06:50 +0000

    ver 1 updated

--- functions/index.js
+++ functions/index.js
@@ -6,69 +6,34 @@ const cors = require('cors');
 admin.initializeApp();
 const app = express();
 app.use(cors());
-// app.use(require('connect').bodyParser());
 const db = admin.firestore();
 
 //Timetables method
 
 //get all timetables
-app.get('/timetables', (req, res) => {
-    (async () => {
-        try {
-            const snapshot = await db.collection('timetables').get();
-            let timetable = [];
-            snapshot.forEach(doc => {
-                let id = doc.id;
-                let data = doc.data();
-                timetable.push(id, data);
-            });
-            return res.status(200).send(timetable);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
-});
-
-//add new timetable
-app.post('/timetables', (req, res) => {
-    (async () => {
-        try {
-            const timetable = req.body;
-            const date = timetable.date;
-            const result = JSON.parse(timetable.field);
-            await db.collection('timetables').doc(date).set(result);
-            res.status(201).send("Success");
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
+app.get('/timetables', async (req, res) => {
+    try {
+        const snapshot = await db.collection('timetables').get();
+        let timetable = [];
+        snapshot.forEach(doc => {
+            let id = doc.id;
+            let data = doc.data();
+            timetable.push(id, data);
+        });
+        return res.status(200).send(timetable);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 });
 
 //get timetable by date
-app.get('/timetables/:date', (req, res) => {
-    (async () => {
-        try {
-            const snapshot = db.collection('timetables').doc(req.params.date);
-            let item = await snapshot.get();
-            let id = item.id;
-            let response = item.data();
-            let timetable = [];
-            timetable.push(id, response);
-            return res.status(200).send(timetable);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
-});
+app.get('/timetables/:date', async (req, res) => {
 
-// //delete timetable by id
-app.delete('/timetables/:timetable_id', async (req, res) => {
     try {
-        const document = db.collection('timetables').doc(req.params.timetable_id);
-        await document.delete();
-        return res.status(200).send();
+        const snapshot = await db.collection('timetables').doc(req.params.date).get();
+        let response = snapshot.data();
+        return res.status(200).send(response);
     } catch (error) {
-        console.log(error);
         return res.status(500).send(error);
     }
 });
@@ -90,45 +55,33 @@ app.get('/students', async (req, res) => {
     }
 });
 
-
 //get student by id
-app.get('/students/:id', (req, res) => {
-    (async () => {
-        try {
-            const snapshot = db.collection('students').doc(req.params.id);
-            let item = await snapshot.get();
-            let id = item.id;
-            let response = item.data();
-            let student = [];
-            student.push(id, response);
-            return res.status(200).send(student);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
+app.get('/students/:id', async (req, res) => {
+    try {
+        const snapshot = await db.collection('students').doc(req.params.id).get();
+        let response = snapshot.data();
+        return res.status(200).send(response);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 });
 
 //get student by email
-app.get('/students/email/:email', (req, res) => {
-    (async () => {
-        try {
-            const snapshot = await db.collection('students').get();
-            let student = [];
+app.get('/students/email/:email', async (req, res) => {
+    try {
+        const snapshot = await db.collection('students').where('Email', '==', req.params.email).get();
+        let student = [];
 
-            snapshot.forEach(doc => {
-                let id = doc.id;
-                let data = doc.data();
-                if (req.params.email === data.Email) {
-                    student.push({ id, data });
-                }
-            });
-            res.status(200).send(student);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
+        snapshot.forEach(doc => {
+            let id = doc.id;
+            let data = doc.data();
+            student.push({ id, data });
+        });
+        res.status(200).send(student);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 });
-
 
 //get all lecturers
 app.get('/lecturers', async (req, res) => {
@@ -147,28 +100,23 @@ app.get('/lecturers', async (req, res) => {
     }
 });
 
-
 //get lecturers by email
-app.get('/lecturers/email/:email', (req, res) => {
-    (async () => {
-        try {
-            const snapshot = await db.collection('lecturers').get();
-            let lecture = [];
+app.get('/lecturers/email/:email', async (req, res) => {
 
-            snapshot.forEach(doc => {
-                let id = doc.id;
-                let data = doc.data();
-                if (req.params.email === data.Email) {
-                    lecture.push({ id, data });
-                }
-            });
-            res.status(200).send(lecture);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
+    try {
+        const snapshot = await db.collection('lecturers').where('Email', '==', req.params.email).get();
+        let lecture = [];
+
+        snapshot.forEach(doc => {
+            let id = doc.id;
+            let data = doc.data();
+            lecture.push({ id, data });
+        });
+        res.status(200).send(lecture);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 });
-
 
 //get all modules
 app.get('/modules', async (req, res) => {
@@ -187,6 +135,62 @@ app.get('/modules', async (req, res) => {
     }
 });
 
+//get classes by module id
+app.get('/classes/module/:id', async (req, res) => {
+    try {
+        const snapshot = await db.collection('classes').where('module_id', '==', parseInt(req.params.id)).get();
+        let classes = [];
+        snapshot.forEach(doc => {
+            let id = doc.id;
+            let data = doc.data();
+            classes.push({ id, data });
+        });
+        if (classes.length > 0) {
+            res.status(200).send(classes);
+        } else {
+            res.status(404).send('not found')
+        }
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+//get course by courseID
+app.get('/courses/:courseID', async (req, res) => {
+    try {
+        const snapshot = await db.collection('courses').doc(req.params.courseID).get();
+        let course = snapshot.data();
+        res.status(200).send(course);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+})
+
+//add new timetable
+app.post('/timetables', async (req, res) => {
+    try {
+        const timetable = req.body;
+        const date = timetable.date;
+        const result = JSON.parse(timetable.field);
+        await db.collection('timetables').doc(date).set(result);
+        res.status(201).send("Success");
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+//add new classes
+app.post('/classes', async (req, res) => {
+    try {
+        const classes = req.body;
+        // const date = classes.date;
+        const result = JSON.parse(classes.field);
+        await db.collection('classes').doc().set(result);
+        res.status(201).send("Success");
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
 
 app.put('/students/:id', async (req, res) => {
     try {
@@ -205,7 +209,7 @@ app.put('/students/:id', async (req, res) => {
             LastName: req.body.LastName,
             MobileNumber: req.body.MobileNumber
         });
-        return res.status(200).send("Update Succesful");
+        return res.status(204).send("Update Succesful");
     } catch (error) {
         console.log(error);
         return res.status(500).send(error);
@@ -218,45 +222,26 @@ app.put('/modules/:id', async (req, res) => {
         await document.update({
             Name: req.body.Name
         });
-        return res.status(200).send(req.body);
+        return res.status(204).send();
     } catch (error) {
         console.log(error);
         return res.status(500).send(req.body);
     }
 });
 
+//update timetable
+app.patch('/timetables/:date/:class_id', async (req, res) => {
+    try {
+        const document = db.collection('timetables').doc(req.params.date);
 
-app.get('/classes', (req, res) => {
-    (async () => {
-        try {
-            const snapshot = await db.collection('classes').get();
-            let timetable = [];
-            snapshot.forEach(doc => {
-                let id = doc.id;
-                let data = doc.data();
-                classes.push(id, data);
-            });
-            return res.status(200).send(classes);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
-});
+        await document.update({
 
-
-//add new timetable
-app.post('/classes', (req, res) => {
-    (async () => {
-        try {
-            const classes = req.body;
-            // const date = classes.date;
-            const result = JSON.parse(classes.field);
-            await db.collection('classes').doc().set(result);
-            res.status(201).send("Success");
-        } catch (error) {
-            return res.status(500).send(error);
-        }
-    })();
+        })
+        return res.status(204).send();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
 });
 
 
@@ -270,15 +255,14 @@ exports.api = functions.https.onRequest(app);
 
 
 ////////=======not tested=============////
-//update timetable
-// app.put('/timetables/:date', async (req, res) => {
+
+
+// ===========not used=============
+// //delete timetable by id
+// app.delete('/timetables/:date', async (req, res) => {
 //     try {
 //         const document = db.collection('timetables').doc(req.params.date);
-//         await document.update({
-//             Classes:[{
-//                 req.body.id
-//             }] req.body.active,
-//         });
+//         await document.delete();
 //         return res.status(200).send();
 //     } catch (error) {
 //         console.log(error);
